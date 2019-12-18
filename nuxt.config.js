091316e57a2244c4558b12b33a3ec3086dcc9a2e
System Administrator <root@MacBook-Air-de-Luis-2.local>
2019-12-18 14:29:47 -0500

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,6 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
